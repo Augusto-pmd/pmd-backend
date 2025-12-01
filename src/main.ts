646adb8602147214b205684f546440ff99448170
PMD Arquitectura <info@pmdarquitectura.com>
2025-12-01 17:35:01 -0300

    fix: backend start command and production build for Render

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
-import cors from 'cors';
+const cors = require('cors');
 import { AppModule } from './app.module';
 
 async function bootstrap() {
