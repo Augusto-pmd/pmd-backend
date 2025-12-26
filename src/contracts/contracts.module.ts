6b0f27d00b9e4c1f95bc3787284a100f090093a8
Rafael <rafael.naranjo19@gmail.com>
2025-12-26 01:45:39 -0500

    refactor: Mejoras menores en DTOs y mÃ³dulos
    - ActualizaciÃ³n de DTOs con validaciones adicionales
    - Ajustes en mÃ³dulos para dependencias

--- src/contracts/contracts.module.ts
+++ src/contracts/contracts.module.ts
@@ -4,10 +4,11 @@ import { ContractsService } from './contracts.service';
 import { ContractsController } from './contracts.controller';
 import { Contract } from './contracts.entity';
 import { Supplier } from '../suppliers/suppliers.entity';
+import { Work } from '../works/works.entity';
 import { AlertsModule } from '../alerts/alerts.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Contract, Supplier]), AlertsModule],
+  imports: [TypeOrmModule.forFeature([Contract, Supplier, Work]), AlertsModule],
   controllers: [ContractsController],
   providers: [ContractsService],
   exports: [ContractsService],
