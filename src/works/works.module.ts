b5c3ccc513561dea16f15f42e558c04716b8494e
Rafael <rafael.naranjo19@gmail.com>
2025-12-28 09:20:17 -0500

    feat(schedule): Completar generaciÃ³n automÃ¡tica de Gantt (Tarea 2.8.1)

--- src/works/works.module.ts
+++ src/works/works.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WorksService } from './works.service';
 import { WorksController } from './works.controller';
@@ -6,9 +6,14 @@ import { Work } from './works.entity';
 import { Organization } from '../organizations/organization.entity';
 import { Expense } from '../expenses/expenses.entity';
 import { Income } from '../incomes/incomes.entity';
+import { Schedule } from '../schedule/schedule.entity';
+import { ScheduleModule } from '../schedule/schedule.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Work, Organization, Expense, Income])],
+  imports: [
+    TypeOrmModule.forFeature([Work, Organization, Expense, Income, Schedule]),
+    forwardRef(() => ScheduleModule),
+  ],
   controllers: [WorksController],
   providers: [WorksService],
   exports: [WorksService],
