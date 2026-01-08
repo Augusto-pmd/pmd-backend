7d54005ea8cc6e6c664028cd0692459b53819b9f
Rafael <rafael.naranjo19@gmail.com>
2026-01-07 21:30:57 -0500

    feat: crear registros de accounting automaticamente al validar incomes - Inyectar AccountingRecord repository en IncomesService - Crear registro de accounting cuando se valida un income - Actualizar validated_by_id y validated_at automaticamente

--- src/incomes/incomes.module.ts
+++ src/incomes/incomes.module.ts
@@ -4,10 +4,11 @@ import { IncomesService } from './incomes.service';
 import { IncomesController } from './incomes.controller';
 import { Income } from './incomes.entity';
 import { Work } from '../works/works.entity';
+import { AccountingRecord } from '../accounting/accounting.entity';
 import { WorksModule } from '../works/works.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Income, Work]), WorksModule],
+  imports: [TypeOrmModule.forFeature([Income, Work, AccountingRecord]), WorksModule],
   controllers: [IncomesController],
   providers: [IncomesService],
   exports: [IncomesService],
